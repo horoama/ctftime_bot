5b4382548d51e6e8124e0007e86e8341d995460e
horoama <horoama929@gmail.com>
2016-11-17 19:02:26 +0900

    create message funtion

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+# coding: utf-8
 import urllib2
-response = urllib2.urlopen('')
-html = response.read()
+import json
+import math
+from datetime import datetime,  timedelta
+
+def get_events(period):
+    baseurl = "https://ctftime.org/api/v1/events/?limit=100"
+    start = datetime.now()
+    fin = start + timedelta(days=period)
+    print fin
+    range_param = "&start={start}&finish={fin}".format(start=start.strftime('%s'), fin=fin.strftime('%s') )
+    response = urllib2.urlopen(baseurl + range_param)
+    res = json.loads(response.read())
+    return res
+
+def away_from(start):
+    now = datetime.now()
+    return start - now  #現在時間との差分を返す
+
+
+def create_message(infos):
+    message = ""
+    for info in infos:
+        if not info['onsite']:
+            start = datetime.strptime(info['start'],'%Y-%m-%dT%H:%M:%S+00:00') + timedelta(hours=9)#時差合わせして
+            d = away_from(start)
+            delta_hours =  int(d.total_seconds()/(60*60))
+            if delta_hours > 48:
+                return message
+            message += info['title'] + u"まで"
+            message += u"あと約{hour}時間({start})\n".format(hour=delta_hours, start=start)
+            message += info['ctftime_url'] + u"\n"
+    return message
+
+infos =  get_events(7)
+
+print create_message(infos)

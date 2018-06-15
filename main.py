3ac40019893da9c4af449f09592a13d719a6d009
horoama <horoama929@gmail.com>
2018-06-16 03:44:04 +0900

    change var name

--- main.py
+++ main.py
@@ -20,27 +20,27 @@ def away_from(start):
     now = datetime.now()
     return start - now  #現在時間との差分を返す
 
-
-def create_message(infos):
+def create_message(upcomings):
     message = ""
-    for info in infos:
-        if not info['onsite']: #onsite形式のみ表示
-            start = datetime.strptime(info['start'],'%Y-%m-%dT%H:%M:%S+00:00') + timedelta(hours=9)#時差合わせして
+    for upcoming in upcomings:
+        if not upcoming['onsite']: #onsite形式のみ表示
+            start = datetime.strptime(upcoming['start'],'%Y-%m-%dT%H:%M:%S+00:00') + timedelta(hours=9)#時差合わせして
             d = away_from(start)
             delta_hours =  int(d.total_seconds()/(60*60))
             if delta_hours > 48:#48時間以上後のイベントは表示させない
                 return message
-            message += info['title'] + u"まで"
+            message += upcoming['title'] + u"まで"
             message += u"あと約{hour}時間({start})\n".format(hour=delta_hours, start=start)
-            message += info['ctftime_url'] + u"\n"
+            message += upcoming['ctftime_url'] + u"\n"
     return message
 
-def send2slack(text):
-    url = os.getenv('SLACK_WEBHOOK_URL')#環境変数にWebhookのURLを入れておく
+def send2slack(text, url=None):
+    if url is None:
+        url = os.getenv('SLACK_WEBHOOK_URL')#環境変数にWebhookのURLを入れておく
     payload = {'text': text, 'username': "ctftime-bot", "icon_emoji": ":robot_face:"}
     res = requests.post(url, data=json.dumps(payload), headers={'Content-Type': 'application/json', 'charset':'utf-8'})
     print(res.content)
 
-infos =  get_events(7) #1週間分取得
-message = create_message(infos)
+upcomings =  get_events(7) #1週間分取得
+message = create_message(upcomings)
 send2slack(message)
